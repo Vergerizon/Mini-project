d07a66d831593bc754cf86510ecddc5288a39f65
Vergerizon <muhammadnoufalkhairy@students.undip.ac.id>
2026-02-05 10:21:46 +0700

    reset fe

--- Frontend/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add web support
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'web.js', 'web.jsx', 'web.ts', 'web.tsx'];
-
-module.exports = config;
