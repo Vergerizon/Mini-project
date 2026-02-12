675e15d2baed892a427a010007d748080ddc82fb
Vergerizon <muhammadnoufalkhairy@students.undip.ac.id>
2026-02-12 09:17:59 +0700

    hadeh

--- Frontend/src/components/common/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return (
-    <header>
-      <h1>DigiWallet</h1>
-    </header>
-  );
-}
--- Frontend/src/services/api.js
+++
@@ -1,6 +0,0 @@
-export const apiBase = process.env.VITE_API_URL || 'http://localhost:3000';
-
-export async function get(path) {
-  const res = await fetch(`${apiBase}${path}`);
-  return res.json();
-}
